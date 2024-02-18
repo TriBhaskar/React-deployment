52e17c56dad1fb153785d6463a6886f5a4e7ff73
TriBhaskar <panthribhaskar@gmail.com>
2024-02-18 11:10:21 +0530

    adding lazy loading for components and loaders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from "./pages/Home";
+//import PostPage, { loader as postLoader } from "./pages/Post";
+import RootLayout from "./pages/Root";
+import { lazy, Suspense } from "react";
 
+const BlogPage = lazy(() => import("./pages/Blog"));
+const PostPage = lazy(() => import("./pages/Post"));
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
@@ -15,10 +18,28 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'posts',
+        path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense>
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense>
+                <PostPage />
+              </Suspense>
+            ),
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)), // module.loader(meta) is the same as module.loader({ params: meta.params }) in this case because the loader function only takes one argument
+          },
         ],
       },
     ],
